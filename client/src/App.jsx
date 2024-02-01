978caf94b530219c2a585abb646a06a76d402490
Carlos Pacheco <carlospacheco.fx@gmail.com>
2024-01-31 20:10:55 -0400

    components folder and Navbar component created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,10 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Routes/Home";
+import Navbar from "./components/navbar";
 const App = () => {
   return (
     <BrowserRouter>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
