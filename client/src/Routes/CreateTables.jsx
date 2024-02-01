5d69a46559d25fc90e1cbedee598a2cf954e84a4
Carlos Pacheco <carlospacheco.fx@gmail.com>
2024-02-01 18:39:01 -0400

    Modification

--- client/src/Routes/CreateTables.jsx
+++ client/src/Routes/CreateTables.jsx
@@ -1,4 +1,23 @@
+import { FilePlus } from "lucide-react";
+
 const CreateTables = () => {
-  return <div>CreateTables</div>;
+  return (
+    <div className="create--tables">
+      <div className="container">
+        <div className="form">
+          <input type="text" placeholder="Nombre de la rifa" />
+          <div className="wrapper">
+            <input type="number" placeholder="Inicio" />
+            <span>Al</span>
+            <input type="number" placeholder="Fin" />
+          </div>
+          <button>
+            <FilePlus />
+            Crear
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 export default CreateTables;
