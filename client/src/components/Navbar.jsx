4161461f6fceba143e129ca6a66f5d0506a3cc3c
Carlos Pacheco <carlospacheco.fx@gmail.com>
2024-01-31 20:25:08 -0400

    components.css created

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,8 +1,13 @@
+import { Menu } from "lucide-react";
 const Navbar = () => {
   return (
     <div className="navbar">
-      <h1>Raffle (rifas jsjs)</h1>
-      <div className="btn--menu"></div>
+      <div className="wrapper">
+        <h1>Raffle (rifas jsjs)</h1>
+        <div className="btn--menu">
+          <Menu />
+        </div>
+      </div>
       <div className="menu--content"></div>
     </div>
   );
