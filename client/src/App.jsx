cb243d53147fa57ce2b8be67b8fc6f89a6cd3e58
Carlos Pacheco <carlospacheco.fx@gmail.com>
2024-02-07 20:58:52 -0400

    Navbar instead of navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Routes/Home";
-import Navbar from "./components/navbar";
+import Navbar from "./components/Navbar";
 import CreateTables from "./Routes/CreateTables";
 const App = () => {
   return (
